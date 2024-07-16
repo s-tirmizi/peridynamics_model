0c386c3dc8c090b5d199e72529c587fb98dbdf11
Syed Talha Tirmizi <106635383+s-tirmizi@users.noreply.github.com>
2024-07-16 17:59:48 -0500

    commit

---
+++ 2d_tutorial.py
@@ -0,0 +1,38 @@
+a = 110
+length = 50
+time = 4
+nodes = 10
+
+dx = length/nodes
+dt = 0.5 * dx**2 / a #stability
+t_nodes = int(time/dt)
+
+u = np.zeros(nodes) + 20
+
+# Boundary conditions
+u[0] = 100
+u[-1] = 100
+
+fig, axis = plt.subplots()
+pcm = axis.pcolormesh([u], cmap = plt.cm.jet, vmin=0, vmax=100)
+plt.colorbar(pcm, ax=axis)
+axis.set_ylim([-2, 3])
+
+
+counter = 0
+
+while counter < time:
+
+    w = u.copy()
+    for i in range(1, nodes-1):
+        u[i] = dt* a * (w[i-1]-2 * w[i] + w[i+1])/dx ** 2 + w[i]
+
+    counter += dt
+
+    print("t: {:.3f} [s], Average temperature: {:.2f} Celcius".format(counter, np.average(u)))
+
+    pcm.set_array([u])
+    axis.set_title("Distribution at t: {:.3f} [s]".format(counter))
+    plt.pause(0.01)
+
+plt.show()
